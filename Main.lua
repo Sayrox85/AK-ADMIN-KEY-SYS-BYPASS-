160360292fa18179cc16b9d56a9f978a328bb404
Nonernicor <160345661+Sayrox85@users.noreply.github.com>
2026-05-18 01:39:11 +0200

    Removed unsupported exec

--- Main.lua
+++ Main.lua
@@ -1,4 +1,4 @@
-local HOOK_CAPABLE = { potassium = true, volt = true, wave = true, madium = true, synapsez = true, sirhurt = true, delta = true, codex= true }
+local HOOK_CAPABLE = { potassium = true, volt = true, wave = true, madium = true, synapsez = true, sirhurt = true }
 
 local function detectExecutor()
     local name = ""
